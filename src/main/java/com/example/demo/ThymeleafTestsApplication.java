050b2ed9ad40a1e02deff92357e3f89537ea7df9
Krystian S <mr.krystian.stawarczyk@gmail.com>
2022-05-31 13:02:17 +0200

    first commit

---
+++ src/main/java/com/example/demo/ThymeleafTestsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafTestsApplication.class, args);
+	}
+
+}
