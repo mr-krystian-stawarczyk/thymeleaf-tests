79975b2b9678f343ba843f46b7611a621bbb6d76
Krystian S <mr.krystian.stawarczyk@gmail.com>
2022-06-04 12:07:10 +0200

    working on a responsivnes, looping cards from mvc working

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,15 +1,24 @@
 package guru.springframework.controllers;
 
+import guru.springframework.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-
-
 
 @Controller
 public class IndexController {
-    @RequestMapping("/")
-    public String getIndex() {
-        return "index";
-    }
+  private ProductService productService;
+
+  @Autowired
+  public void setProductService(ProductService productService) {
+    this.productService = productService;
+  }
+
+  @RequestMapping("/")
+  public String getIndex(Model model) {
+    model.addAttribute("products", productService.listProducts());
+
+    return "index";
+  }
 }
